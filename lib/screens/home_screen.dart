5a620baa4722a6e3b8536d1e41ddb355633309f9
Jef <jef.overweg2@gmail.com>
2023-02-28 13:14:44 +0100

    feat: commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import 'movie_list.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -11,24 +13,49 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final user = FirebaseAuth.instance.currentUser!;
 
-  void signOut(){
+  void signOut() {
     FirebaseAuth.instance.signOut();
+  }
+
+  void navigateToMovieList() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) => MovieList()));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [IconButton(onPressed: signOut, icon: Icon(Icons.logout))],
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                child: Text('My top rated'),
+                value: 1,
+              ),
+            ],
+            icon: Icon(Icons.list),
+            onSelected: (value) {
+              // Handle menu item selection
+              if (value == 1) {
+                navigateToMovieList();
+              }
+            },
+          ),
+          IconButton(
+            onPressed: signOut,
+            icon: Icon(Icons.logout),
+          ),
+        ],
       ),
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Signed in'),
-          Text('User : ' + user.email!),
-        ],
-      )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Signed in'),
+            Text('User : ' + user.email!),
+          ],
+        ),
+      ),
     );
   }
 }
