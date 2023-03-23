5bc9d9cf47af311f606fe31bc424cd44561d00cf
lorenzo30salgado <lorenzo.dintino@student.uclouvain.be>
2023-03-23 12:32:41 +0100

    permet de choisire le provider

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -5,10 +5,16 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class ResultScreen extends StatefulWidget {
-  const ResultScreen({Key? key,required this.aventure, required this.action, required this.comedie}) : super(key: key);
+  const ResultScreen({Key? key,required this.aventure, required this.action, required this.comedie, required this.crime, required this.drama, required this.fantasy,required this.horror,required this.scifi, required this.providers}) : super(key: key);
   final double aventure;
   final double action;
   final double comedie;
+  final double crime;
+  final double drama;
+  final double fantasy;
+  final double horror;
+  final double scifi;
+  final providers;
   @override
   State<ResultScreen> createState() => ResultScreenSate();
 
@@ -18,12 +24,12 @@ class ResultScreenSate extends State<ResultScreen> {
   final apiKey = '9478d83ca04bd6ee25b942dd7a0ad777';
   Color mainColor = const Color(0xff3C3261);
   List<dynamic> movies = [];
-
   Future<void> getData() async {
     final user = FirebaseAuth.instance.currentUser!;
     final snapshot = await FirebaseFirestore.instance.collection('movies').doc(user.uid).get();
     List<dynamic>? moviesId = snapshot.data()!['movies_id'];
     List<dynamic> moviesData = [];
+    List<dynamic> moviesDataTitels = [];
     List<dynamic> Genres = [];
     if(widget.aventure>=50){
       Genres.add(12);
@@ -34,24 +40,61 @@ class ResultScreenSate extends State<ResultScreen> {
     if(widget.comedie>=50){
       Genres.add(35);
     }
+    if(widget.crime>=50){
+      Genres.add(80);
+    }
+    if(widget.drama>=50){
+      Genres.add(18);
+    }
+    if(widget.fantasy>=50){
+      Genres.add(14);
+    }
+    if(widget.horror>=50){
+      Genres.add(27);
+    }
+    if(widget.scifi>=50){
+      Genres.add(878);
+    }
 
     if(moviesId != null){
       for (String movieId in moviesId) {
-          final result = await http.get(Uri.parse('https://api.themoviedb.org/3/movie/$movieId/recommendations?api_key=$apiKey&language=en-US&page=1'));
+        //get a list of recommend movies based on seen movies
+        final result = await http.get(Uri.parse('https://api.themoviedb.org/3/movie/$movieId/recommendations?api_key=$apiKey&language=en-US&page=1'));
           if (result.statusCode == 200) {
             final Map<String, dynamic> resultData = json.decode(result.body);
             for(int i=0;i<resultData['results'].length;i++){
+              //check if the movie recommended has the same genre as set in the preferences
               if(resultData['results'][i]['genre_ids'].length != 0 && Genres.contains(resultData['results'][i]['genre_ids'][0] as int)){
+                //check if the movie recommended is not in the seen movies list
                 if(!moviesId.contains(resultData["results"][i]["id"].toString())) {
-                  moviesData.add(resultData["results"][i]);
+                  String recId = resultData["results"][i]["id"].toString();
+                  //get the providers list of the recommended movie
+                  final movieProvider = await http.get(Uri.parse('https://api.themoviedb.org/3/movie/$recId/watch/providers?api_key=$apiKey'));
+                  if(movieProvider.statusCode == 200){
+                    final Map<String, dynamic> ProviderData = json.decode(movieProvider.body);
+                    //check if the movie has any provider in Belgium
+                    if(ProviderData["results"]["BE"] != null && ProviderData["results"]["BE"]["flatrate"] != null){
+                      for(int y=0; y<ProviderData["results"]["BE"]["flatrate"].length;y++){
+                        // check if the provider is in the providers list
+                        print(ProviderData["results"]["BE"]["flatrate"][y]["provider_name"]);
+                        if(widget.providers[ProviderData["results"]["BE"]["flatrate"][y]["provider_name"]] ==1){
+                          //check if movie added not in list of movie data
+                          if(!moviesDataTitels.contains(resultData["results"][i]["title"])){
+                            moviesDataTitels.add(resultData["results"][i]["title"]);
+                            moviesData.add(resultData["results"][i]);
+                          }
+                          break;
+                        }
+                      }
+                    }
+
+                  }
                 }
               }
             }
           }
-      }
-      print(moviesData.first["id"]);
+        }
     }
-
     setState(() {
       movies = moviesData;
     });
--- lib/screens/salons.dart
+++ lib/screens/salons.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:night_solver/screens/preference_page.dart';
+import 'package:night_solver/screens/watch_providers.dart';
 import 'new_salon_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -102,7 +102,7 @@ class _SalonsState extends State<Salons> {
                   return MaterialButton(
                     child: SalonCell(salons[i]),
                     padding: const EdgeInsets.all(0.0),
-                    onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => Preferences())),
+                    onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => Providers())),
                     color: Colors.white,
                   );
                 },
