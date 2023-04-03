d8eb624834fddb5290c702c4a22bc2e8bb3070c3
duboissim <simon.y.dubois@student.uclouvain.be>
2023-04-03 21:39:16 +0200

    Home_page Advancment + added asset, colors and theme + custom movie_infoClass + custom vertical and horizontal movie card

---
+++ lib/theme/app_style.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+
+import '../utils/color_constant.dart';
+import '../utils/size_utils.dart';
+
+class AppStyle {
+  static TextStyle txtPoppinsRegular16Bluegray400 = TextStyle(
+    color: ColorConstant.blueGray400,
+    fontSize: getFontSize(
+      16,
+    ),
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle txtPoppinsBold30 = TextStyle(
+    color: ColorConstant.whiteA700,
+    fontSize: getFontSize(
+      30,
+    ),
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle txtGilroyRegular12 = TextStyle(
+    color: ColorConstant.gray700,
+    fontSize: getFontSize(
+      12,
+    ),
+    fontFamily: 'Gilroy',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle txtPoppinsBold20 = TextStyle(
+    color: ColorConstant.whiteA700,
+    fontSize: getFontSize(
+      20,
+    ),
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle txtPoppinsRegular14 = TextStyle(
+    color: ColorConstant.whiteA700,
+    fontSize: getFontSize(
+      14,
+    ),
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle txtPoppinsRegular16 = TextStyle(
+    color: ColorConstant.redA700,
+    fontSize: getFontSize(
+      16,
+    ),
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle txtPoppinsRegular16WhiteA700 = TextStyle(
+    color: ColorConstant.whiteA700,
+    fontSize: getFontSize(
+      16,
+    ),
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle txtPoppinsBold36 = TextStyle(
+    color: ColorConstant.whiteA700,
+    fontSize: getFontSize(
+      36,
+    ),
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle txtPoppinsMedium22 = TextStyle(
+    color: ColorConstant.whiteA700,
+    fontSize: getFontSize(
+      22,
+    ),
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle txtPoppinsRegular12 = TextStyle(
+    color: ColorConstant.redA700,
+    fontSize: getFontSize(
+      12,
+    ),
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle txtPoppinsRegular13 = TextStyle(
+    color: ColorConstant.blueGray400,
+    fontSize: getFontSize(
+      13,
+    ),
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle txtPoppinsRegular14Gray900 = TextStyle(
+    color: ColorConstant.gray900,
+    fontSize: getFontSize(
+      14,
+    ),
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle txtRobotoRegular16 = TextStyle(
+    color: ColorConstant.bluegray400,
+    fontSize: getFontSize(
+      16,
+    ),
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle txtPoppinsBold20WhiteA700 = TextStyle(
+    color: ColorConstant.whiteA700,
+    fontSize: getFontSize(
+      20,
+    ),
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle txtPoppinsSemiBold18 = TextStyle(
+    color: ColorConstant.whiteA700,
+    fontSize: getFontSize(
+      18,
+    ),
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle txtPoppinsMedium18 = TextStyle(
+    color: ColorConstant.whiteA700,
+    fontSize: getFontSize(
+      18,
+    ),
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle txtPoppinsBold36WhiteA700 = TextStyle(
+    color: ColorConstant.whiteA700,
+    fontSize: getFontSize(
+      36,
+    ),
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle txtRobotoRegular20 = TextStyle(
+    color: ColorConstant.black901,
+    fontSize: getFontSize(
+      20,
+    ),
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle txtPoppinsMedium16 = TextStyle(
+    color: ColorConstant.gray900,
+    fontSize: getFontSize(
+      16,
+    ),
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+  );
+}
