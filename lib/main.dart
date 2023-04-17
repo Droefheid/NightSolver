3e97bbc71d10c6e8b545b61875c1059081c0f241
lorenzo30salgado <lorenzo.dintino@student.uclouvain.be>
2023-04-17 13:31:21 +0200

    correction de bug

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
         '/' : (context) => MainScreen(),
